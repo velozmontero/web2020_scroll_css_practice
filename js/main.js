ec5f55217bfde4ed5dcb34d10057b5823f491b35
Frank Veloz <velozmontero@gmail.com>
2018-06-13 18:35:20 -0400

    improved dropping

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ window.onscroll = function (e) {
   if (document.body.scrollTop < 600 || document.documentElement.scrollTop < 600){
 
     document.getElementById("crazy_boy").style.backgroundPosition = '0px 0px';
-    document.getElementById("crazy_boy").style.marginBottom = '50%';
+    document.getElementById("crazy_boy").style.top = '-80px';
   }
 
-  if (document.body.scrollTop > 600 && document.body.scrollTop < 1200 || 
-      document.documentElement.scrollTop > 600 && document.documentElement.scrollTop < 1200 ) {
+  if (document.body.scrollTop > 50 && document.body.scrollTop < 1200 || 
+      document.documentElement.scrollTop > 50 && document.documentElement.scrollTop < 1200 ) {
 
     console.log('scrolled here ', document.body.scrollTop, document.documentElement.scrollTop);
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-71px 0px';
-    document.getElementById("crazy_boy").style.marginBottom = '50%';
+    document.getElementById("crazy_boy").style.top = '50%';
   } 
 
   if (document.body.scrollTop > 1200 && document.body.scrollTop < 1800 ||
@@ -20,7 +20,7 @@ window.onscroll = function (e) {
     console.log('scrolled here ', document.body.scrollTop, document.documentElement.scrollTop);
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-142px 0px';
-    document.getElementById("crazy_boy").style.marginBottom = '50%';
+    document.getElementById("crazy_boy").style.top = '50%';
   }
 
   if (document.body.scrollTop > 1800 && document.body.scrollTop < 2400 ||
@@ -29,7 +29,7 @@ window.onscroll = function (e) {
     console.log('scrolled here ', document.body.scrollTop, document.documentElement.scrollTop);
 
     document.getElementById("crazy_boy").style.backgroundPosition = '0px -80px';
-    document.getElementById("crazy_boy").style.marginBottom = '50%';
+    document.getElementById("crazy_boy").style.top = '50%';
   }
 
   if (document.body.scrollTop > 2400 && document.body.scrollTop < 3000 ||
@@ -38,7 +38,7 @@ window.onscroll = function (e) {
     console.log('scrolled here ', document.body.scrollTop, document.documentElement.scrollTop);
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-71px -80px';
-    document.getElementById("crazy_boy").style.marginBottom = '50%';
+    document.getElementById("crazy_boy").style.top = '50%';
   }
 
   if (document.body.scrollTop > 3000 && document.body.scrollTop < 3600 ||
@@ -47,10 +47,10 @@ window.onscroll = function (e) {
     console.log('scrolled here ', document.body.scrollTop, document.documentElement.scrollTop);
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-142px -80px';
-    document.getElementById("crazy_boy").style.marginBottom = '50%';
+    document.getElementById("crazy_boy").style.top = '50%';
   }
   else if (document.body.scrollTop > 3600 || document.documentElement.scrollTop > 3600) {
     document.getElementById("crazy_boy").style.backgroundPosition = '0px -160px';
-    document.getElementById("crazy_boy").style.marginBottom = '150px';
+    document.getElementById("crazy_boy").style.top = '75%';
   }
 } 
